31530eb1a109cd17390c728d2dc7c907095bdaa0
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-28 22:28:16 -0600

    addedContact.jsFunction

---
+++ src/components/contact.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { validateEmail } from '../utils/validateEmail'
+
+const titleStyle = {
+  backgroundColor: 'red',
+  padding: '20px'
+}
+
+const linkStyle = {
+  color: 'red',
+}
+
+const padding = {
+  padding: '10px'
+}
+
+const errorStyle = {
+  backgroundColor: 'black',
+  color: 'red'
+}
+
