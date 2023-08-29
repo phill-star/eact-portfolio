cfb504acd3e704868adaffb8876645f5ff57c24b
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-28 22:55:32 -0600

    addedAReturnfunctionForContact

--- src/components/contact.js
+++ src/components/contact.js
@@ -46,4 +46,45 @@ function Contact() {
 
     function handleSubmit(e) {
       e.preventDefault();
-    }};
+    }
+
+
+    return(
+        <div class="contact" style={padding}>
+        <h1 style={titleStyle}>Contact Me</h1>
+        <form>
+          <div class="form-group" style={padding}>
+            <label htmlFor="name">Your name:</label>
+            <input type="text" class="form-control" id="name" placeholder="Leave your name here..." defaultValue={name} onBlur={handleChange}/>
+          </div>
+          <div class="form-group" style={padding}>
+            <label htmlFor="email">Your email:</label>
+            <textarea name="email" id="email" placeholder="Enter your email here..." class="form-control" defaultValue={email} onBlur={handleChange}></textarea>
+          </div>
+          <div class="form-group" style={padding}>
+            <label htmlFor="message">Your message:</label>
+            <textarea name="message" id="message" placeholder="Enter your message here..." class="form-control" defaultValue={message} onBlur={handleChange}></textarea>
+          </div>
+
+          {errorMessage && (
+            <div style={errorStyle}>
+              <h3>Form Errors:</h3>
+              <p class="error">{errorMessage}</p>
+            </div>
+          )}
+
+          <button class="btn btn-dark" style={linkStyle} onSubmit={handleSubmit}>Submit Message</button>
+        </form>
+        <p style={padding}>Alternatively, please feel free to contact me directly thru either of the avenues below. </p>
+          
+          <h5>
+            Call me at <a href="tel:610.608.0403" style={linkStyle}>763-221-6590</a>
+          </h5>
+          <h5>
+            Email me at <a href="mailto:aungvary5@gmail.com" style={linkStyle}>philadelphia536@yahoo.com</a>
+          </h5>
+      </div>
+    )
+}
+
+export default Contact;
