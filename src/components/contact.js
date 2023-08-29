53191cedd5b8d059822bbae99b079db92b3f9c2a
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-28 22:39:51 -0600

    addedContactFunction

--- src/components/contact.js
+++ src/components/contact.js
@@ -19,3 +19,31 @@ const errorStyle = {
   color: 'red'
 }
 
+function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' })
+
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+      if (e.target.name === 'email') {
+        const validate = validateEmail(e.target.value);
+
+        if (!validate) {
+          setErrorMessage('Please enter a valid email.');
+        } else {
+          setErrorMessage('')
+        }
+      } else if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`)
+      } else setErrorMessage();
+
+      if (!errorMessage) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+      }
+    }
+
+    function handleSubmit(e) {
+      e.preventDefault();
+    }};
