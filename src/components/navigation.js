bb3ffebcceb9c178ea577cc71abc1c0275faa5a5
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-29 20:37:36 -0600

    adedNavReturnFunction

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -13,3 +13,29 @@ const activeLink = {
 const barStyle = {
   width: '100%'
 }
+
+function Navigation(props) {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark" style={barStyle}>
+    <a class="navbar-brand" style={navStyle} href="#">Andrew Ungvary</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+      <NavLink to="/about"><a class="nav-link" style={navStyle}>About Me</a></NavLink>
+        <NavLink to="/portfolio"><a class="nav-link" style={navStyle}>Portfolio</a></NavLink>
+
+        {/* <a class="nav-link" href="https://github.com/phill-star?tab=repositories">GitHub</a>
+        <a class="nav-link" href="https://www.linkedin.com/in/mohamed-hussein-569aa828a/>LinkedIn</a> */}
+
+        <NavLink to="/resume"><a class="nav-link" style={navStyle}>Resume</a></NavLink>
+        <NavLink to="/contact"><a class="nav-link" style={navStyle}>Contact</a></NavLink>
+
+      </div>
+    </div>
+  </nav>
+    );
+}
+
+export default Navigation;
