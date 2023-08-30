17aff7fe8ead90482a32e36b2b6c189f716e672b
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-29 20:21:18 -0600

    adedNavImporrtFunction

---
+++ src/components/navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const navStyle = {
+  color: 'red'
+}
+
+const activeLink = {
+  fontWeight: 'bold'
+}
+
+const barStyle = {
+  width: '100%'
+}
