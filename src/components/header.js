c5deaad0431d47e0d5d6c085f8a30cbd9e5fc34c
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-29 20:06:02 -0600

    addedHeaderpath

--- src/components/header.js
+++ src/components/header.js
@@ -6,5 +6,23 @@ import Contact from '../components/contact'
 import Navigation from '../components/navigation'
 import Resume from '../components/resume'
 
+class Header extends Component {
+    render() {
+        return(
+            <HashRouter>
+                <div className="nav">
+                    <Navigation/>
+                </div>
+            <div className="routes">
+                <Route exact path="/" render={() => (<Redirect to="/about"/>)}/>
+                <Route path="/portfolio" component={Portfolio}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="/resume" component={Resume}/>
+            </div>
+            </HashRouter>
+        )
+    }
+}
 
 export default Header;
