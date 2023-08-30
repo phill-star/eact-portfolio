fffb9561941c6379a0d39d773fc49e39c335c411
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-29 19:51:34 -0600

    addedImportan

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Route, Redirect, HashRouter} from 'react-router-dom';
+import Portfolio from '../components/portfolio'
+import About from '../components/about'
+import Contact from '../components/contact'
+import Navigation from '../components/navigation'
+import Resume from '../components/resume'
+
+
+export default Header;
