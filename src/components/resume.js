7f951ac446e4b2ce54237a43c22e3bac89d202f1
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-09-02 06:42:05 -0600

    addedResumeFunction

--- src/components/resume.js
+++ src/components/resume.js
@@ -13,3 +13,31 @@ const titleStyle = {
   backgroundColor: 'red',
   padding: '20px'
 }
+
+function Resume() {
+    return(
+        <section class="mb-4" style={padding}>
+            {/* <h1 className="resume" style={titleStyle}>RESUME</h1> */}
+
+            <h2 className="skills" style={titleStyle}>FRONT END SKILLS</h2>
+            
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>jQuery</p>
+            <p>IndexedDB</p>
+
+            <h2 className="skills" style={titleStyle}>BACK END SKILLS</h2>
+            
+            <p>Express.js</p>
+            <p>SQL (mySQL, sequelize)</p>
+            <p>noSQL (mongoDB, mongoose)</p>
+            <p>APIs (RESTful, server-side, third-party)</p>
+
+            <button type="button" class="btn btn-dark"><a href="https://profile.indeed.com/document/view " style={linkStyle}>View Full Resume</a></button>
+            
+          
+        </section>
+    )
+}
+
+export default Resume;
