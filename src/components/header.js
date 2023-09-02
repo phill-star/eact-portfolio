2829f2f49d3f66ae01e0edd993415ac7092d4371
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-09-02 10:12:09 -0600

    editedHeadShotSize

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,30 @@
-import React, {Component} from 'react';
-import {Route, Redirect, HashRouter} from 'react-router-dom';
-import Portfolio from '../components/portfolio'
-import About from '../components/about'
-import Contact from '../components/contact'
-import Navigation from '../components/navigation'
-import Resume from '../components/resume'
+import React, { Component } from 'react';
+import { Routes, Route, HashRouter } from 'react-router-dom'; // Import Routes and Route
+import Portfolio from '../components/portfolio';
+import About from '../components/about';
+import Contact from '../components/contact';
+import Navigation from '../components/navigation';
+import Resume from '../components/resume';
 
 class Header extends Component {
-    render() {
-        return(
-            <HashRouter>
-                <div className="nav">
-                    <Navigation/>
-                </div>
-            <div className="routes">
-                <Route exact path="/" render={() => (<Redirect to="/about"/>)}/>
-                <Route path="/portfolio" component={Portfolio}/>
-                <Route path="/about" component={About}/>
-                <Route path="/contact" component={Contact}/>
-                <Route path="/resume" component={Resume}/>
-            </div>
-            </HashRouter>
-        )
-    }
+  render() {
+    return (
+      <HashRouter>
+        <div className="nav">
+          <Navigation />
+        </div>
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default Header;
