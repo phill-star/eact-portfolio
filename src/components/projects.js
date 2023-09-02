97d9f60406df3202c35850e14c39cc09249373c4
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-09-02 06:13:08 -0600

    addedPorjectFile

---
+++ src/components/projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const info = {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '10px'
+}
+
+const titleStyle = {
+    backgroundColor: 'red',
+    padding: '10px'
+}
+
+const linkStyle = {
+    color: 'red'
+}
