7e60084b7563de230245bc98a260ca1576f3d60a
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-09-02 06:31:45 -0600

    addedCard

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,5 +12,33 @@ const titleStyle = {
 }
 
 const linkStyle = {
-    color: 'red'
+    color: 'red',
 }
+
+function ProjectCards(props) {
+    return (
+        <div className="card">
+            {}
+
+            <div className="cardContent">
+                <h2 className="projectTitle" id="projectTitle" style={titleStyle}>{props.name}</h2>
+
+                <div className="img-container">
+                <img class="projectImage" alt={props.name} src={props.image}/>
+                </div>
+
+                <p className="projectInfo" id="projectInfo" style={info}>
+                <button class="btn btn-dark"><a href={props.deploy} style={linkStyle}>Deployed Application</a></button>
+                <br></br>
+                <button class="btn btn-dark"><a href={props.github} style={linkStyle}>Application Repository</a></button>
+                </p>
+                <p className="technologies">
+                    <h4>Techologies Used: </h4>
+                    {props.technologies}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCards;
