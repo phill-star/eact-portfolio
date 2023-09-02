9f3fa2cc7c2a1a2f602cc106d7dcd2a8b53f7838
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-09-02 06:35:05 -0600

    addedRsumeFile

---
+++ src/components/resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const linkStyle = {
+  color: 'red',
+  padding: '20px'
+}
+
+const padding = {
+  padding: '10px'
+}
+
+const titleStyle = {
+  backgroundColor: 'red',
+  padding: '20px'
+}
