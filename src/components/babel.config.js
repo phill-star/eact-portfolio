73ca151de96cd1329021e348b10a89e5ece67a15
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-09-06 17:21:40 -0600

    addedbevel

---
+++ src/components/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    presets: [
+      // Your other presets
+      'react-app'
+    ],
+    plugins: [
+      // Your other plugins
+      '@babel/plugin-proposal-private-property-in-object'
+    ]
+  };
